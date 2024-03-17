82e4a5037a8c079a7e296f8ebeaf74e1c6b548c0
Boset <boss-x86@protonmail.com>
2024-03-17 17:35:42 -0600

    login-screen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
+// Angular
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+// Screens
+import { LoginScreenComponent } from './screens/login-screen/login-screen.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginScreenComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/screens/login-screen/login-screen.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+//JQuery
+declare var $: any;
+
+@Component({
+  selector: 'app-login-screen',
+  templateUrl: './login-screen.component.html',
+  styleUrls: ['./login-screen.component.scss']
+})
+export class LoginScreenComponent implements OnInit{
+
+  public type: String = "password";
+  public username: String = "";
+  public password: String = "";
+
+  public errors:any = {};
+
+  constructor(
+    private router: Router
+  ) {}
+
+  ngOnInit():void{
+  
+  }
+
+  public login(){
+
+  }
+
+  public showPassword(){
+    if(this.type == "password"){
+      $("#show-password").addClass("show-password");
+      $("#show-password").attr("data-password", true);
+      this.type = "text";
+    }else if(this.type == "text"){
+      $("#show-password").removeClass("show-password");
+      $("#show-password").attr("data-password", false);
+      this.type = "password";
+    }
+  }
+
+  public goRegistro(){
+    this.router.navigate(["registro-usuario"]);
+  }
+}
