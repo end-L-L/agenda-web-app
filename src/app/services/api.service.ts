0e16a85d2b5c8378fe47e57c19ab4a754378395d
Boset <boss-x86@protonmail.com>
2024-03-23 22:12:40 -0600

    api services

---
+++ src/app/services/api.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  /* Servicios HTTP */
+
+  // Registar Usuario
+  public registrarUsuario (data: any): Observable <any>{
+    return this.http.post<any>(`${environment.url_api}/register/`,data, httpOptions);
+  }
+}
